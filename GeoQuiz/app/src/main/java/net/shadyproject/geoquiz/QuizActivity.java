491fcacf2ce65de6aa9373d07321b3ba344f4e7e
Christopher Martin <christopher.martin2@nike.com>
2015-09-16 10:37:29 -0700

    lifecycle logging

--- GeoQuiz/app/src/main/java/net/shadyproject/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/net/shadyproject/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package net.shadyproject.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
 
     private Button _trueButton;
     private Button _falseButton;
@@ -30,6 +33,8 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_quiz);
 
         _trueButton = (Button)findViewById(R.id.true_button);
@@ -59,6 +64,36 @@ public class QuizActivity extends AppCompatActivity {
 
         _questionTextView = (TextView)findViewById(R.id.question_text_view);
         updateQuestion();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
